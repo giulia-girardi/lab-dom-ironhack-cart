a0248d190a64dbf906177c477d940e8f452466cd
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-10-12 16:40:10 +0200

    finish iteration2

--- js/index.js
+++ js/index.js
@@ -13,15 +13,10 @@ function updateSubtotal(product) {
   return subTotalField;
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+  function calculateAll() {
+    const allProducts = document.querySelectorAll('.product');
+    allProducts.forEach(pizza => updateSubtotal(pizza))
 
   // ITERATION 3
   //... your code goes here
